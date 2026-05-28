a2ae6ac938f0c49b07b9925e3f19a9243549cade
SoClear <48470201+SoClear@users.noreply.github.com>
2026-05-29 02:01:58 +0800

    feat: add android.os.SystemProperties stub

---
+++ stub/src/main/java/android/os/SystemProperties.java
@@ -0,0 +1,10 @@
+package android.os;
+
+public class SystemProperties {
+    private SystemProperties() {
+    }
+
+    public static int getInt(String tag, int defaultValue) {
+        throw new RuntimeException("Stub!");
+    }
+}
